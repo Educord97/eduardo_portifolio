779b605b4c714007a00dd23bb17e2b7dd9205d81
Eduardo <eduuhliima97@gmail.com>
2020-11-09 11:06:56 -0300

    terminando

--- js/script.js
+++ js/script.js
@@ -25,6 +25,18 @@
 
   */
 
+//Skills
+
+$("#skills_jq_btn").click(function(){
+  $(".box_skills_jq").css("display", "block");
+
+})
+
+  
+
+
+
+
 //Portifólio
 
   $("#img_jq_box").on({
@@ -74,6 +86,54 @@
   $("#img_jq_box_6").mouseleave(function(){
       $(".overlay_img_6").hide(450);
   })
+
+    //Box Wordpress
+
+  $("#img_jq_box_w1").mouseenter(function(){
+    $(".img_w_1").show(450)
+  })
+  $("#img_jq_box_w1").mouseleave(function(){
+      $(".img_w_1").hide(450);
+  })
+
+  $("#img_jq_box_w2").mouseenter(function(){
+    $(".img_w_2").show(450)
+  })
+  $("#img_jq_box_w2").mouseleave(function(){
+      $(".img_w_2").hide(450);
+  })
+
+  $("#img_jq_box_w3").mouseenter(function(){
+    $(".img_w_3").show(450)
+  })
+  $("#img_jq_box_w3").mouseleave(function(){
+      $(".img_w_3").hide(450);
+  })
+
+  $("#img_jq_box_w4").mouseenter(function(){
+    $(".img_w_4").show(450)
+  })
+  $("#img_jq_box_w4").mouseleave(function(){
+      $(".img_w_4").hide(450);
+  })
+
+  $("#img_jq_box_w5").mouseenter(function(){
+    $(".img_w_5").show(450)
+  })
+  $("#img_jq_box_w5").mouseleave(function(){
+      $(".img_w_5").hide(450);
+  })
+
+  $("#img_jq_box_w6").mouseenter(function(){
+    $(".img_w_6").show(450)
+  })
+  $("#img_jq_box_w6").mouseleave(function(){
+      $(".img_w_6").hide(450);
+  })
+
+
+
+
 
 
 
